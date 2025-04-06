d127fa821677e9c05f5bc4be8175ca00f08951b9
lacerdaaa <edulacerdaaa@gmail.com>
2025-04-05 21:33:38 -0300

    feat(): initializing project with spring initializer

---
+++ src/test/java/com/lacerda/desafioitau/DesafioItauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lacerda.desafioitau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
