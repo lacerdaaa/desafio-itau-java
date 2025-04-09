1bff02630a4ac4e4c0a61670e6030de06a0f497d
lacerdaaa <edulacerdaaa@gmail.com>
2025-04-08 23:58:47 -0300

    descondiderar

--- src/main/java/com/lacerda/desafioitau/service/TransactionService.java
+++ src/main/java/com/lacerda/desafioitau/service/TransactionService.java
@@ -2,6 +2,7 @@ package com.lacerda.desafioitau.service;
 
 import com.lacerda.desafioitau.domain.Transaction;
 import com.lacerda.desafioitau.dto.TransactionRequest;
+import com.lacerda.desafioitau.service.validators.Validator;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,8 +12,14 @@ import java.util.List;
 public class TransactionService {
 
     private final List<Transaction> transactions = new ArrayList<>();
+    private final List<Validator> validations;
 
-    public void salvar( TransactionRequest request) {
+    public TransactionService(List<Validator> validations) {
+        this.validations = validations;
+    }dadadasda
+
+    public void salvar(TransactionRequest request) {
+        validations.forEach(v -> v.validate(request));
         transactions.add(new Transaction(request.valor(), request.dataHora()));
     }
 }
